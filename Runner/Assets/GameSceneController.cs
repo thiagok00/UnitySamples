36a4a9850a0134aa175c37ab4c9426eed1023a4d
Thiago Angelis <thiago_angelis@CCESP.PUC-RIO.BR>
2017-07-27 18:54:36 -0300

    runner

---
+++ Runner/Assets/GameSceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneController : MonoBehaviour {
+
+	public Player player;
+	public Camera gameCamera;
+	public GameObject[] blockPrefabs;
+
+	void Start () {
+		Instantiate (blockPrefabs[0]);
+	}
+
+	void Update () {
+		if (player != null)
+			gameCamera.transform.position = new Vector2(player.transform.position.x, gameCamera.transform.position.y);
+	}
+}
---
+++ Runner/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public int jumpForce;
+	public int moveVelocity;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown("space")) {
+			this.GetComponent<Rigidbody2D>().AddForce( new Vector2(0,jumpForce));
+		}
+		this.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveVelocity , this.GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		Debug.Log (c.tag);
+		if (c.tag == "Obstacle") {
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
